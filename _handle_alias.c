c604b1e08895d839cfddbcf8cd5337a9690e2d46
Lance Desk <lncedesk@gmail.com>
2023-09-15 14:04:54 -0700

    Correction of Betty issues - 11th attempt

--- _handle_alias.c
+++ _handle_alias.c
@@ -1,16 +1,90 @@
 #include "lance.h"
 #include "helpers.h"
 
+void _print_all_aliases(void);
+Alias *_find_alias_by_name(const char *name);
+void _initialize_aliases(void);
 
 /* An array to store alias mappings */
 Alias aliases[MAX_ALIASES];
 
+
+/**
+ * _handle_alias - Handle the alias builtin prompt.
+ *
+ * @prompt_args: An array of prompt arguments.
+ */
+
+void _handle_alias(char **prompt_args)
+{
+	char *name, *value;
+
+	if (prompt_args[1] == NULL)
+	{
+		/* No args provided, print a list of all aliases */
+		_print_all_aliases();
+	}
+	else if (prompt_args[2] == NULL || _strcmp(prompt_args[2], "=") == 0)
+	{
+		/* One argument provided or an equal sign (=) */
+		/* as the second argument, */
+		/* treat it as an alias name without a value */
+		name = prompt_args[1];
+		Alias *alias = _find_alias_by_name(name);
+
+		if (alias != NULL)
+		{
+			/* Print the alias without single quotes */
+			printf("%s %s\n", alias->name, alias->value);
+		}
+		else
+		{
+			/* Alias not found */
+			perror("Alias '%s' not found.\n", name);
+		}
+	}
+	else
+	{
+		/* Two or more arguments provided, */
+		/* define or redefine an alias */
+		name = prompt_args[1];
+		value = prompt_args[2];
+
+		Alias *alias = _find_alias_by_name(name);
+
+		if (alias != NULL)
+		{
+			/* Alias with the same name found, */
+			/* update its value */
+			/* Free the previous value and name */
+			free(alias->value);
+			free(alias->name);
+			alias->value = _strdup(value);
+			/* Assign the new name */
+			alias->name = _strdup(name);
+		}
+		else
+		{
+			/* Alias with the same name not found, */
+			/* create a new alias */
+			for (int i = 0; i < MAX_ALIASES; i++)
+			{
+				if (aliases[i].name == NULL)
+				{
+					aliases[i].name = _strdup(name);
+					aliases[i].value = _strdup(value);
+					return;
+				}
+			}
+
+			/* Maximum number of aliases reached */
+			perror("Maximum number of aliases reached.\n");
+		}
+	}
+}
+
 /**
  * _initialize_aliases - Initialize the aliases array.
- *
- * This function initializes the array of aliases,
- * setting all elements to NULL
- * to prepare it for storing alias mappings.
  */
 
 void _initialize_aliases(void)
@@ -25,91 +99,39 @@ void _initialize_aliases(void)
 }
 
 /**
- * _handle_alias - Handle the alias builtin prompt.
- *
- * This function parses the arguments for the alias
- * prompt and performs the specified actions
- * (e.g., defining or listing aliases).
- *
- * @prompt_args: An array of prompt arguments.
+ * _print_all_aliases - Print a list of all aliases.
  */
 
-void _handle_alias(char **prompt_args)
+void _print_all_aliases(void)
 {
 	int i;
-	char *name, *value;
 
-	if (prompt_args[1] == NULL)
+	for (i = 0; i < MAX_ALIASES; i++)
 	{
-		/* No args provided, print a list of all aliases */
-		for (i = 0; i < MAX_ALIASES; i++)
+		if (aliases[i].name != NULL)
 		{
-			if (aliases[i].name != NULL)
-			{
-				printf("%s %s\n", aliases[i].name, aliases[i].value);
-			}
+			printf("%s %s\n", aliases[i].name, aliases[i].value);
 		}
 	}
-	else if (prompt_args[2] == NULL || strcmp(prompt_args[2], "=") == 0)
+}
+
+/**
+ * _find_alias_by_name - Find an alias by name.
+ *
+ * @name: The name of the alias to find.
+ * Return: A pointer to the alias if found, NULL otherwise.
+ */
+
+Alias *_find_alias_by_name(const char *name)
+{
+	int i;
+
+	for (i = 0; i < MAX_ALIASES; i++)
 	{
-		/* One argument provided or an equal sign (=) */
-		/* as the second argument, */
-		/* treat it as an alias name without a value */
-		name = prompt_args[1];
-
-		for (i = 0; i < MAX_ALIASES; i++)
+		if (aliases[i].name != NULL && _strcmp(aliases[i].name, name) == 0)
 		{
-			if (aliases[i].name != NULL && _strcmp(aliases[i].name, name) == 0)
-			{
-				/* Print the alias without single quotes */
-				value = aliases[i].value;
-				printf("%s %s\n", aliases[i].name, value);
-				return;
-			}
+			return (&aliases[i]);
 		}
-		/* Alias not found */
-		fprintf(stderr, "Alias '%s' not found.\n", name);
 	}
-	else
-	{
-		/* Two or more arguments provided, */
-		/* define or redefine an alias */
-		name = prompt_args[1];
-		value = prompt_args[2];
-
-		/* Check if an alias with the same */
-		/* name already exists */
-		for (i = 0; i < MAX_ALIASES; i++)
-		{
-			if (aliases[i].name != NULL && _strcmp(aliases[i].name, name) == 0)
-			{
-				/* Alias with the same name found, */
-				/* update its value */
-				/* Free the previous value */
-				free(aliases[i].value);
-				/* Free the previous name */
-				free(aliases[i].name);
-				aliases[i].value = _strdup(value);
-				/* Assign the new name */
-				aliases[i].name = _strdup(name);
-				return;
-			}
-		}
-
-
-		/* Alias with the same name not found, */
-		/* create a new alias */
-		for (i = 0; i < MAX_ALIASES; i++)
-		{
-			if (aliases[i].name == NULL)
-			{
-				aliases[i].name = _strdup(name);
-				aliases[i].value = _strdup(value);
-				return;
-			}
-		}
-
-		/* Maximum number of aliases reached */
-		fprintf(stderr, "Maximum number of aliases reached.\n");
-	}
+	return (NULL);
 }
