9945d2e679a3d58d90cc96d50b7368ac64873cf0
Avumile <Smithavery435@gmail.com>
2023-09-23 10:38:30 -0700

    user defined getline function

---
+++ _getline.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+static char buffer[BUFFER_SIZE];
+static int buffer_index = 0;
+
+/**
+ * my_getline - Custom getline function with buffer for efficient character reading.
+ * Returns a dynamically allocated string containing the user's input line.
+ * Caller is responsible for freeing the memory allocated for the string.
+ * Returns NULL on EOF or error.
+ */
+char *my_getline(void)
+{
+	int line_index = 0;
+	char c;
+	ssize_t bytes_read;
+	char *line;
+
+	line = malloc(BUFFER_SIZE);
+	if (!line)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1)
+	{
+		if (buffer_index == 0)
+		{
+			bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+			if (bytes_read <= 0) 
+			{
+				free(line);			return (NULL);
+			}
+			buffer_index = bytes_read;
+		}
+
+		c = buffer[buffer_index - 1];
+		buffer_index--;
+
+		if (c == '\n')
+		{
+			line[line_index] = '\0';
+			return (line);
+		}
+		else
+		{
+			line[line_index] = c;
+			line_index++;
+		}
+	}
+}
+
