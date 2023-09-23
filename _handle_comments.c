2bee1dcad1c934e594eecdd6ec5ab0b9d5613c91
Lance Desk <lncedesk@gmail.com>
2023-09-23 03:15:38 -0700

    Correcting comment handler

--- _handle_comments.c
+++ _handle_comments.c
@@ -5,44 +5,26 @@
  * @input: User input as a string.
  * Return: Returns 1 if the line contains a comment
  * and has been handled, 0 otherwise.
-*/
+ */
 
 int _handle_comments(char *input)
 {
 	int i;
 	int in_comment = 0;
-	char *comment = NULL;
 
 	for (i = 0; input[i] != '\0'; i++)
 	{
-		if (input[i] == '#' && !in_comment)
+		if (input[i] == '#' && (i == 0 || input[i - 1] == ' '))
 		{
 			in_comment = 1;
+			input[i] = '\0'; /* Swap '#' with '\0' */
 		}
 		else if (input[i] != ' ' && in_comment)
 		{
 			in_comment = 0;
 		}
 	}
-
-	/* If the entire line is a comment, return 1. */
-	if (in_comment)
-	{
-		return (1);
-	}
-
-	/* Remove the comment from the input string. */
-	while (*input == ' ')
-	{
-		input++;
-	}
-
-	comment = strchr(input, '#');
-	if (comment != NULL)
-	{
-		*comment = '\0';
-	}
-
-	return (0);
+	/* Return 1 if a comment was handled, 0 otherwise */
+	return (in_comment);
 }
 
